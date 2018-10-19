73c2b32839432a16ae1de8845bab33af81fbb1d9
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-10-19 14:40:31 -0700

    update workflow, new gulpfile

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//     var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+//     url +=
+//       "?" +
+//       $.param({
+//         "api-key": "42ab6a5236364b609385636b553df245"
+//       });
+
+//     $("button").on("click", function() {
+//       $.ajax({
+//         url: url,
+//         method: "GET"
+//       })
+//         .done(function(data) {
+//           // $('.results').empty();
+//           $('.results').append('<p>' + data.home.[U.S] + '</p>');
+//     })
+
+//         .fail(function(err) {
+//           throw err;
+//         });
+//     });
+
+//   }); //END OF DOC READY
